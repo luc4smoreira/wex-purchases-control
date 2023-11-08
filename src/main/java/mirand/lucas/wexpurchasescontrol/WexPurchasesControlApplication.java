1271bc918dfb170923e564d92ffc9045a2160339
Lucas Moreira Carneiro de Miranda <lucas@hastatus.com.br>
2023-11-08 11:42:47 -0300

    Initial commit with Spring Initializr

---
+++ src/main/java/mirand/lucas/wexpurchasescontrol/WexPurchasesControlApplication.java
@@ -0,0 +1,13 @@
+package mirand.lucas.wexpurchasescontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WexPurchasesControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WexPurchasesControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mirand/lucas/wexpurchasescontrol/WexPurchasesControlApplicationTests.java
@@ -0,0 +1,13 @@
+package mirand.lucas.wexpurchasescontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WexPurchasesControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
