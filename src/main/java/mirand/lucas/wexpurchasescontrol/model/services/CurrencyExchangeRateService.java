b0f72b5e12e485a4be3d46babca3ac4fd7133458
Lucas Moreira Carneiro de Miranda <lucas@hastatus.com.br>
2023-11-08 13:43:16 -0300

    Implemented connection to retrieve currency exchange rates using api.fiscaldata.treasury.gov.

--- src/main/java/mirand/lucas/wexpurchasescontrol/model/services/CurrencyExchangeRateService.java
+++ src/main/java/mirand/lucas/wexpurchasescontrol/model/services/CurrencyExchangeRateService.java
@@ -1,10 +1,26 @@
 package mirand.lucas.wexpurchasescontrol.model.services;
 
+import jakarta.validation.constraints.NotNull;
+import mirand.lucas.wexpurchasescontrol.dto.DateIntervalDTO;
+import mirand.lucas.wexpurchasescontrol.exceptions.CurrencyExchangeNotAvailableException;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public interface CurrencyExchangeRateService {
 
-    BigDecimal getCurrencyExchangeRateByDate(final String currency, final LocalDate date);
+
+    /**
+     * Retrieves the most recent exchange rate for the specified currency within the given date interval.
+     *
+     * @param country Country associated with a given exchange rate. (e.g., "Brazil", "Australia")
+     * @param currency Currency associated with a given exchange rate. (e.g., "Real", "Dollar")
+     * @param intervalDTO An object containing the start and end dates defining the interval for the query.
+     * @return The most recent {@link BigDecimal} exchange rate for the specified currency in the given interval.
+     * @throws CurrencyExchangeNotAvailableException if the specified currency is not found.
+     */
+    BigDecimal getNewestExchangeRateInInterval(@NotNull final String country, @NotNull final String currency, @NotNull DateIntervalDTO intervalDTO) throws CurrencyExchangeNotAvailableException;
+
+    DateIntervalDTO generateAcceptableInterval(@NotNull LocalDate purchaseDate);
 
 }
