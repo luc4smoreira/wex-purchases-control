01c5f82bffb20f046e255b7d351535d204c81b0f
Lucas Moreira Carneiro de Miranda <lucas@hastatus.com.br>
2023-11-08 15:19:55 -0300

    Improvements on Javadoc for CurrencyExchangeRateService

--- src/main/java/mirand/lucas/wexpurchasescontrol/model/services/CurrencyExchangeRateService.java
+++ src/main/java/mirand/lucas/wexpurchasescontrol/model/services/CurrencyExchangeRateService.java
@@ -15,12 +15,18 @@ public interface CurrencyExchangeRateService {
      *
      * @param country Country associated with a given exchange rate. (e.g., "Brazil", "Australia")
      * @param currency Currency associated with a given exchange rate. (e.g., "Real", "Dollar")
-     * @param intervalDTO An object containing the start and end dates defining the interval for the query.
+     * @param intervalDTO An object containing the start and end dates defining the acceptable date interval for the exchange rate related to the given purchase
      * @return The most recent {@link BigDecimal} exchange rate for the specified currency in the given interval.
      * @throws CurrencyExchangeNotAvailableException if the specified currency is not found.
      */
     BigDecimal getNewestExchangeRateInInterval(@NotNull final String country, @NotNull final String currency, @NotNull DateIntervalDTO intervalDTO) throws CurrencyExchangeNotAvailableException;
 
+    /**
+     * Generates an acceptable date interval for the exchange rate related to a given purchase date.
+     *
+     * @param purchaseDate the date of the purchase transaction
+     * @return a {@code DateIntervalDTO} object containing the start and end dates that define the acceptable interval for the exchange rate
+     */
     DateIntervalDTO generateAcceptableInterval(@NotNull LocalDate purchaseDate);
 
 }
