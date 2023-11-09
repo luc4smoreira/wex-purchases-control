5954bbbe3986698855e1e4ece80f6f96f26679b7
Lucas Moreira Carneiro de Miranda <lucas@hastatus.com.br>
2023-11-09 10:15:55 -0300

    Add Unit tests for TreasureGovExchangeRateServiceImp

--- src/test/java/mirand/lucas/wexpurchasescontrol/model/service/TreasureGovExchangeRateServiceImpTest.java
+++ src/test/java/mirand/lucas/wexpurchasescontrol/model/service/TreasureGovExchangeRateServiceImpTest.java
@@ -5,31 +5,48 @@ import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 import mirand.lucas.wexpurchasescontrol.dto.DateIntervalDTO;
+import mirand.lucas.wexpurchasescontrol.dto.ExchangeRateDataDTO;
+import mirand.lucas.wexpurchasescontrol.dto.ExchangeRateResponseDTO;
 import mirand.lucas.wexpurchasescontrol.model.services.imp.TreasureGovExchangeRateServiceImp;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.client.RestTemplate;
 
+import java.math.BigDecimal;
+import java.net.URI;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
-@SpringBootTest
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class TreasureGovExchangeRateServiceImpTest {
 
-    @MockBean
+    private static final String COUNTRY_BRAZIL = "Brazil";
+    private static final String CURRENCEY_REAL = "Real";
+    @Mock
     private RestTemplate restTemplate;
 
     private TreasureGovExchangeRateServiceImp service;
 
 
+    @BeforeEach
+    void setUp() {
+        service = new TreasureGovExchangeRateServiceImp(restTemplate);
+    }
+
+
 
     @Test
     public void testAcceptableInterval() {
-
-
-        service = new TreasureGovExchangeRateServiceImp(restTemplate);
 
         DateIntervalDTO dateIntervalDTO = service.generateAcceptableInterval(LocalDate.now());
 
@@ -52,9 +69,6 @@ public class TreasureGovExchangeRateServiceImpTest {
     @Test
     public void testFutureInterval() {
 
-        service = new TreasureGovExchangeRateServiceImp(restTemplate);
-
-
         DateIntervalDTO dateIntervalDTO = service.generateAcceptableInterval(LocalDate.now().plusDays(1));
 
 
@@ -62,39 +76,44 @@ public class TreasureGovExchangeRateServiceImpTest {
         Validator validator = factory.getValidator();
         Set<ConstraintViolation<DateIntervalDTO>> violations = validator.validate(dateIntervalDTO);
 
-        Assertions.assertTrue(!violations.isEmpty());
+        Assertions.assertFalse(violations.isEmpty());
 
     }
 
     @Test
-    public void testApiCall() {
+    public void testGetNewestExchangeRateInIntervalReturnSuccessfull() {
 
-//        BigDecimal expected = new BigDecimal(1.42);
-//        // Configurar o mock do RestTemplate para retornar um resultado esperado
-//        ExchangeRateResponseDTO exchangeRateResponseDTO = new ExchangeRateResponseDTO();
-//
-//        ExchangeRateDataDTO exchangeRateDataDTO = new ExchangeRateDataDTO();
-//        exchangeRateDataDTO.setExchangeRate(expected);
-//
-//        List<ExchangeRateDataDTO> data = new ArrayList<>();
-//        data.add(exchangeRateDataDTO);
-//
-//        exchangeRateResponseDTO.setData(data);
-//
-//        RestTemplate mockRestTemplate = mock(RestTemplate.class);
-//        when(mockRestTemplate.getForObject(any(String.class), any())
-//        ).thenReturn(exchangeRateResponseDTO);
-//
-//
-//        when(restTemplateBuilderMock.build()).thenReturn(mockRestTemplate);
-//
-//        service = new TreasureGovExchangeRateServiceImp(restTemplateBuilderMock);
-//
-//
-//        DateIntervalDTO dateIntervalDTO = service.generateAcceptableInterval(LocalDate.now());
-//
-//        BigDecimal result = service.getNewestExchangeRateInInterval("Brazil", "Real", dateIntervalDTO);
-//        Assertions.assertEquals(0, expected.compareTo(result));
+        BigDecimal expected = new BigDecimal(1.42);
+        // Configurar o mock do RestTemplate para retornar um resultado esperado
+        ExchangeRateResponseDTO exchangeRateResponseDTO = new ExchangeRateResponseDTO();
+
+        ExchangeRateDataDTO exchangeRateDataDTO = new ExchangeRateDataDTO();
+        exchangeRateDataDTO.setExchangeRate(expected);
+
+        List<ExchangeRateDataDTO> data = new ArrayList<>(List.of(exchangeRateDataDTO));
+
+
+        exchangeRateResponseDTO.setData(data);
+
+        when(restTemplate.getForObject(any(URI.class), eq(ExchangeRateResponseDTO.class))).thenReturn(exchangeRateResponseDTO);
+
+
+        DateIntervalDTO dateIntervalDTO = service.generateAcceptableInterval(LocalDate.now());
+
+        BigDecimal result = service.getNewestExchangeRateInInterval(COUNTRY_BRAZIL, CURRENCEY_REAL, dateIntervalDTO);
+        Assertions.assertEquals(0, expected.compareTo(result));
+
+    }
+
+    @Test
+    public void testGetNewestExchangeRateInIntervalReturnNull() {
+
+        when(restTemplate.getForObject(any(URI.class), eq(ExchangeRateResponseDTO.class))).thenReturn(null);
+
+        DateIntervalDTO dateIntervalDTO = service.generateAcceptableInterval(LocalDate.now());
+
+        BigDecimal result = service.getNewestExchangeRateInInterval(COUNTRY_BRAZIL, CURRENCEY_REAL, dateIntervalDTO);
+        Assertions.assertEquals(null, result);
 
     }
 
