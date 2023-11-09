6d637c1947c056169056e431401fa9313dc8a766
Lucas Moreira Carneiro de Miranda <lucas@hastatus.com.br>
2023-11-09 20:32:54 -0300

    Add HTTP Response status with reason for exceptions created

--- src/main/java/mirand/lucas/wexpurchasescontrol/exceptions/CurrencyExchangeNotAvailableException.java
+++ src/main/java/mirand/lucas/wexpurchasescontrol/exceptions/CurrencyExchangeNotAvailableException.java
@@ -1,5 +1,10 @@
 package mirand.lucas.wexpurchasescontrol.exceptions;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.NOT_FOUND, reason="The purchase cannot be converted to the target currency. To access only the original purchase transaction, leave the country and currency fields empty.")
 public class CurrencyExchangeNotAvailableException extends RuntimeException{
 
 }
