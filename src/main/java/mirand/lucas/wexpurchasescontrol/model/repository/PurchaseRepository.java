65849ae574949c8b59bea477c91c68d124f9c155
Lucas Moreira Carneiro de Miranda <lucas@hastatus.com.br>
2023-11-10 17:45:33 -0300

    Removing class repositoy, using DAO instead

--- src/main/java/mirand/lucas/wexpurchasescontrol/model/repository/PurchaseRepository.java
+++
@@ -1,9 +0,0 @@
-package mirand.lucas.wexpurchasescontrol.model.repository;
-
-import mirand.lucas.wexpurchasescontrol.model.entity.PurchaseTransaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PurchaseRepository extends JpaRepository<PurchaseTransaction, Long> {
-}
