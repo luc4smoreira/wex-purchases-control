784d89e13d9dec6dff1212e4d5b7a7d6822decf7
Lucas Moreira Carneiro de Miranda <lucas@hastatus.com.br>
2023-11-10 16:02:18 -0300

    Disable integration tests for now.

--- src/test/java/mirand/lucas/wexpurchasescontrol/WexPurchasesControlApplicationTests.java
+++ src/test/java/mirand/lucas/wexpurchasescontrol/WexPurchasesControlApplicationTests.java
@@ -3,10 +3,10 @@ package mirand.lucas.wexpurchasescontrol;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class WexPurchasesControlApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
